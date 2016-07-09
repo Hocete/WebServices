9243e4a4c7b9723cf6ca3757e83b8287a6872454
dai sy <516093398@qq.com>
2016-07-09 16:13:55 +0800

    添加项目文件。

---
+++ WebServices/Controllers/WebController.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections.Generic;
+using WebServices.Models;
+using System.Web.Http;
+
+namespace WebServices.Controllers
+{
+    public class WebController : ApiController
+    {
+        private ReservationRepository repo = ReservationRepository.Current;
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return repo.GetAll();
+        }
+        public Reservation GetReservation(int id)
+        {
+            return repo.Get(id);
+        }
+        [HttpPost]
+        public Reservation CreateReservation(Reservation item)
+        {
+            return repo.Add(item);
+        }
+        [HttpPut]
+        public bool UpdateReservation(Reservation item)
+        {
+            return repo.Update(item);
+        }
+        // GET: Web
+        public void DeleteReservation(int id)
+        {
+            repo.Remove(id);
+        }
+    }
+}
---
+++ WebServices/Models/ReservationRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServices.Models
+{
+    public class ReservationRepository
+    {
+        private static ReservationRepository repo = new ReservationRepository();
+        public static ReservationRepository Current
+        {
+            get
+            {
+                return repo;
+            }
+        }
+        private List<Reservation> data = new List<Reservation>
+        {
+            new Reservation {ReservationId=1,ClientName="Adam",Location="AoardDoom" },
+            new Reservation {ReservationId=2,ClientName="Bdam",Location="BoardCoom" },
+            new Reservation {ReservationId=3,ClientName="Cdam",Location="CoardBoom" },
+            new Reservation {ReservationId=4,ClientName="Dam",Location="DoardAoom" }
+        };
+        public IEnumerable<Reservation> GetAll()
+        {
+            return data;
+        }
+        public Reservation Get(int id)
+        {
+            return data.Where(r => r.ReservationId == id).FirstOrDefault();
+        }
+        public Reservation Add(Reservation iteam)
+        {
+            iteam.ReservationId = data.Count + 1;
+            data.Add(iteam);
+            return iteam;
+        }
+        public void Remove(int id)
+        {
+            Reservation item = Get(id);
+            if (item != null)
+                data.Remove(item);
+        }
+        public bool Update(Reservation item)
+        {
+            Reservation storedItem = Get(item.ReservationId);
+            if (storedItem != null)
+            {
+                storedItem.ClientName = item.ClientName;
+                storedItem.Location = item.Location;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
